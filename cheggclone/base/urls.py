bf2a67f74c8d23b6e780daeb0a5102ff3d7b8292
robinmuhia <muhiarobinonyancha@gmail.com>
2023-04-03 17:35:06 +0300

    Added Update to CRUD functionality

--- cheggclone/base/urls.py
+++ cheggclone/base/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('room/<str:pk>/',views.room,name='room'),
-    path('create-room/',views.create_room,name='create-room')
+    path('create-room/',views.create_room,name='create-room'),
+    path('update-room/<str:pk>/',views.update_room,name='update-room'),
 ]
--- cheggclone/base/views.py
+++ cheggclone/base/views.py
@@ -27,3 +27,17 @@ def create_room(request:str):
         
     context = {'form':form}
     return render(request,'base/room_form.html',context)
+
+
+def update_room(request:str, pk:str):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    context = {'form':form}
+    return render(request,'base/room_form.html',context)
