64000abb31b4922e0cbddbdae5b382fe3ba07d6a
robinmuhia <muhiarobinonyancha@gmail.com>
2023-04-03 15:35:52 +0300

    Added templates to base folder

--- cheggclone/base/views.py
+++ cheggclone/base/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def home(request):
-    return render(request,'home.html')
+rooms = [
+    {'id': 1,
+    'name':"Let's learn python"},
+    {'id': 2,
+    'name':"Design with me #React,Mui,Next"},
+    {'id': 3,
+    'name':"Looking for frontend devs"}
+
+]
+
+def home(request:str):
+    context = {'rooms':rooms}
+    return render(request,'base/home.html',context)
 
 
-def room(request):
-    return render(request,'room.html')
+def room(request:str,pk:str):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    
+    context = {'room':room}
+            
+    return render(request,'base/room.html',context)
