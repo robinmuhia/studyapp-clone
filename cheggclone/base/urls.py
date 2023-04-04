05f1064a8152994620b80fd39194442ca4cc2cc6
robinmuhia <muhiarobinonyancha@gmail.com>
2023-04-04 15:05:34 +0300

    Added registration page and register functionality

--- cheggclone/base/urls.py
+++ cheggclone/base/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('login/',views.login_page,name='login'),
     path('logout/',views.logout_user,name='logout'),
+    path('register/',views.register_user,name='register'),
     path('',views.home,name='home'),
     path('room/<str:pk>/',views.room,name='room'),
     path('create-room/',views.create_room,name='create-room'),
--- cheggclone/base/views.py
+++ cheggclone/base/views.py
@@ -7,14 +7,18 @@ from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 
 def login_page(request:object):
+    page = 'login'
+    
     if request.user.is_authenticated:
-        return redirect('home')
+        return redirect('home')  
+    
     
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
             user = User.objects.get(username = username)
@@ -28,13 +32,31 @@ def login_page(request:object):
         else:
             messages.error(request,'Username or password does not match with the records in our database')
             
-    context ={}
+    context ={'page':page }
     return render(request,'base/login_register.html',context)    
     
 
 def logout_user(request:object):
     logout(request)
     return redirect('home')
+    
+
+def register_user(request:object):
+    form = UserCreationForm()
+    
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration!!')
+        
+    context ={'form' : form}
+    return render(request,'base/login_register.html',context)
     
     
 def home(request:object):
