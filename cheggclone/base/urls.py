6621774637b41d6a97beaf3802900b2ad15e54a0
robinmuhia <muhiarobinonyancha@gmail.com>
2023-04-04 14:15:53 +0300

    added Login functionality

--- cheggclone/base/urls.py
+++ cheggclone/base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('login/',views.login_page,name='login'),
     path('',views.home,name='home'),
     path('room/<str:pk>/',views.room,name='room'),
     path('create-room/',views.create_room,name='create-room'),
--- cheggclone/base/views.py
+++ cheggclone/base/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render,redirect
 from django.db.models import Q
 from .models import Room,Topic
+from django.contrib.auth.models import User
 from .forms import RoomForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
-
+def login_page(request:object):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username = username)
+        except:
+            messages.error(request,'User does not exist')
+            
+        user = authenticate(request,username = username,password = password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'Username or password does not match with the records in our database')
+            
+    context ={}
+    return render(request,'base/login_register.html',context)    
+    
+    
+    
 def home(request:object):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
