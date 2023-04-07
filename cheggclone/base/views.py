ae7396177428bccbb991d29928990a79fd26dba1
robinmuhia <muhiarobinonyancha@gmail.com>
2023-04-07 22:25:24 +0300

    added mobile responsiveness

--- cheggclone/base/views.py
+++ cheggclone/base/views.py
@@ -176,3 +176,14 @@ def update_user(request:object):
             return redirect('user-profile',pk=user.id)
     return render(request,'base/update_user.html',{'form':form})
 
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
+
