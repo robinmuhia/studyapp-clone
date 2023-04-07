bd45128536eb87959625d899bf41b5b76d78a5da
robinmuhia <muhiarobinonyancha@gmail.com>
2023-04-07 20:32:45 +0300

    added ability to edit user profile

--- cheggclone/base/views.py
+++ cheggclone/base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.db.models import Q
 from .models import Room,Topic,Message
 from django.contrib.auth.models import User
-from .forms import RoomForm
+from .forms import RoomForm,UserForm
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
@@ -163,4 +163,16 @@ def delete_message(request:object,pk:str):
         room_message.delete()
         return redirect('home')
     return render(request,'base/delete.html',{'obj':room_message})
+
+
+@login_required(login_url='login')
+def update_user(request:object):
+    user = request.user
+    form = UserForm(instance=user)
+    if request.method == "POST":
+        form = UserForm(request.POST,instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile',pk=user.id)
+    return render(request,'base/update_user.html',{'form':form})
 
